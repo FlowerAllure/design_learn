ef6c8a06f2e3fd50cd71a18aa5b74a5549a307a7
feng.pan <feng.pan@mail.com>
2022-01-04 09:28:03 +0800

    提交年前的代码

---
+++ src/Pool/StringReverseWorker.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Flowerallure\DesignLearn\Pool;
+
+class StringReverseWorker
+{
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function run(string $text)
+    {
+        return strrev($text);
+    }
+}
